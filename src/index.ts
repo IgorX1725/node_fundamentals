efb1eebf987413b2cbb97a4ceaecf162e15da0fe
Igor Cortez <ibarbos2@its.jnj.com>
2024-10-29 15:42:56 -0300

    adding transaction routes

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
 import Fastify from 'fastify'
-import { knex } from './database'
+
+
 import { env } from './env'
+import { transactionsRoutes } from './routes/transactions'
 
 const app = Fastify({
   logger: true,
 })
 
-app.get('/', async () => {
-  const transactions = await knex('transactions').select('*')
-
-  return transactions
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
 })
 
 app.listen({ port: env.PORT }, (err, address) => {
