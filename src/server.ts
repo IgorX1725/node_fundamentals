0bcbf1ac0c00ddbf230566f6917c057d5b957842
Igor Cortez <ibarbos2@its.jnj.com>
2024-10-31 10:38:07 -0300

    adding host property on server configuration

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
 import { app } from './app'
 import { env } from './env'
 
-app.listen({ port: env.PORT }, (err, address) => {
-  if (err) throw err
-  console.log(`Server is now listening on ${address}`)
-})
+app.listen(
+  { port: env.PORT, host: 'RENDER' in process.env ? '0.0.0.0' : 'localhost' },
+  (err, address) => {
+    if (err) throw err
+    console.log(`Server is now listening on ${address}`)
+  },
+)
